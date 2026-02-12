34aae021b170f8d46b4f941ff3e67fa4e1a03b52
HALLOWHANGER <ibrahimasilmansur@gmail.com>
2026-02-12 12:56:01 +0100

    final commit made by asil for now

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -4,7 +4,7 @@ import { auth } from "@clerk/nextjs/server";
 
 export async function createClient() {
   const cookieStore = await cookies();
-
+// what is helping us to fetch data from supbase and manage session and cookies  
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
